52a9ab54e5824d3d243b9d753397fea34adae094
IsamBenS <isam.ben-soltane@inserm.fr>
2018-10-24 12:22:56 +0200

    v1

--- inst/shinyApp/app/server.R
+++ inst/shinyApp/app/server.R
@@ -51,7 +51,7 @@ server <- function(input, output, session)
     )
     
     env.var <- reactiveValues(
-        tool.wd = getwd()
+        tool.wd = system.file("shinyApp", "app", package = "AnalysisTool")
     )
     
     write.enriched.FCS <- function(fcs, fcs.path)
